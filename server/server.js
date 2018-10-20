12e9d4ff81f089a8c990dab0c51d5dc28af3152d
Kevin W <wongkokwah@gmail.com>
2018-10-20 22:47:02 +0800

    Init commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+
+const publicPath = path.join(__dirname, "../public");
+
+// console.log(__dirname + "/../public");
+// console.log(publicPath);
+
+var app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+app.listen(port, ()=> {
+ console.log(`Startup on port: ${port}`);
+});
