3bdfe858e057506533c98b6c73e9bd24d74e6703
Kevin W <wongkokwah@gmail.com>
2018-10-21 16:52:59 +0800

    Fix minor bug

--- server/server.js
+++ server/server.js
@@ -3,9 +3,9 @@ const http = require("http");
 const express = require("express");
 const socketIO = require("socket.io");
 
-const publicPath = path.join(__dirname, "../public");
+const publicPath = path.join(__dirname, "/../public");
 // console.log(__dirname + "/../public");
-// console.log(publicPath);
+console.log(publicPath);
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
